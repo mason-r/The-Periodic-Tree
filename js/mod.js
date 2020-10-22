7ec75f52eb22842ed91f41aa5f87b29102dd8faf
thepaperpilot <thepaperpilot@gmail.com>
2020-10-21 19:06:40 -0500

    Version bump

--- js/mod.js
+++ js/mod.js
@@ -12,8 +12,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.1.1",
-	name: "Cash Influx [rebalanced]",
+	num: "0.2",
+	name: "Row 3",
 }
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
