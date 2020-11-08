1626de20fdc6b5a6200bf6c24244e6e2efdf972e
thepaperpilot <thepaperpilot@gmail.com>
2020-11-08 17:54:30 -0600

    Fixed layOver treating Decimals like Objects

--- js/utils.js
+++ js/utils.js
@@ -577,7 +577,7 @@ function showTab(name) {
 
 function layOver(obj1, obj2) {
 	for (let x in obj2) {
-		if (obj2[x] instanceof Object) layOver(obj1[x], obj2[x]);
+		if (obj2[x] instanceof Object && !(obj2[x] instanceof Decimal)) layOver(obj1[x], obj2[x]);
 		else obj1[x] = obj2[x];
 	}
 }
