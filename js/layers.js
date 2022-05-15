0277c2feaa6a9505cb76c800043c30526b8bd969
mason-r <89530346+mason-r@users.noreply.github.com>
2022-05-15 15:20:21 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,15 +1,15 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("q", {
+    name: "quarks", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "Q", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+        points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#BBBBBB",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "quarks", // Name of prestige currency
+    baseResource: "particles", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -22,14 +22,23 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "q", description: "Q: Reset for quarks", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
     upgrades: {
         11: {
-            name: "Please work please pleaseeeee",
-            description: "oh man please work dude please",
-            cost: new Decimal(0),
-        }
-    }
+            name: "Faster Particles",
+            description: 'Triple particle gain.',
+            cost: new Decimal(2),
+        },
+        12: {
+            name: 'THe J',
+            description: 'Wifjfijij',
+            cost: new Decimal(2),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() {return format(upgradeEffect(this.layer, this.id)) + 'x'},
+        },
+    },
 })
